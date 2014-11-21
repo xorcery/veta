1fa5fda377d61e52bb7469f587c32357519d3966
Bryce Hammond <bryce.hammond@imulus.com>
2014-11-21 12:37:06 -0700

    Move to jquery datepicker instead of rails date selects

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
 //= require bootstrap-sprockets
 //= require best_in_place
 //= require best_in_place.jquery-ui
@@ -20,5 +21,8 @@
 
 $(document).ready(function() {
   /* Activating Best In Place */
-  jQuery(".best_in_place").best_in_place();
+  $(".best_in_place").best_in_place();
+  /* Active date pickers */
+  $("input.date_picker").datepicker();
+
 });
