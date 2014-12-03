702269baaf0494de7173430c55fa290962bc0bb3
Bryce Hammond <bryce.hammond@imulus.com>
2014-12-02 23:13:12 -0700

    Switch coloring on stage change

--- app/assets/javascripts/pipeline.js
+++ app/assets/javascripts/pipeline.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+    $('.stage-select').bind("ajax:success", function () {
+        var $rowElement = $(this).closest('tr');
+        switch(this.innerHTML) {
+            case "Won":
+                $rowElement.attr("class", "success");
+                break;
+            case "Lost":
+                $rowElement.attr("class","danger");
+                break;
+            default:
+               $rowElement.removeAttr("class"); 
+        }
+
+    });
+
+});
