fa58aaabe2247e8833a985c18441515f7f6f50ef
Bryce Hammond <bryce.hammond@imulus.com>
2014-12-02 22:54:40 -0700

    Add helper to highlight rows as appropriate based on status

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,10 @@
 module ProjectsHelper
+  def row_classes_for_project(project)
+    if ['Won','Lost'].include? project.stage
+      highlight_class = project.stage == 'Won' ? 'success' : 'danger'
+      "class=\"#{highlight_class}\"".html_safe 
+    else
+      ''
+    end
+  end
 end
