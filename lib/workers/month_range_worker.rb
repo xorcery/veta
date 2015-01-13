4e8633b4b14238e7bf67ad2724c3081b05fd6c0f
Bryce Hammond <bryce.hammond@imulus.com>
2015-01-13 16:46:37 -0700

    Finish out logic on timeframes for pipeline processing in concurrency

--- lib/workers/month_range_worker.rb
+++ lib/workers/month_range_worker.rb
@@ -10,12 +10,11 @@ class MonthRangeWorker
     result.save!
 
     month = result.month
-    current_and_future_projects = Project.where("start_on >= ?", month)
-    pipeline_projects = current_and_future_projects.where("stage <> 'Won'").where("probability > 0")
-    backlog_projects = current_and_future_projects.where(stage: 'Won')
+    pipeline_projects = Project.where("stage <> 'Won'").where("probability > 0")
+    backlog_projects = Project.where(stage: 'Won')
 
     #filter down to active projects
-    pipeline_projects.select! { |project| project.start_on.advance(months: project.months) <= month }
+    pipeline_projects.select! { |project| project.start_on.advance(months: project.months) >= month and project.start_on.beginning_of_month <= month }
     backlog_projects.select! {|project| project.backlog_for_month(month).percent_complete.to_f > 0.0 }
 
     backlog_amount = backlog_amount_for_projects(backlog_projects, month)
@@ -43,18 +42,19 @@ class MonthRangeWorker
   end
 
   def backlog_amount_for_project(project, month)
+    backlog_amount = 0
     backlog_month = project.backlog_for_month(month)
     if backlog_month.present? and backlog_month.percent_complete.to_f > 0
       #get the percent difference from the prior month
       prior_month = project.backlog_for_month(month.advance(months: -1))
       if prior_month.present? and prior_month.percent_complete.to_f > 0
-        (backlog_month.percent_complete - prior_month.percent_complete) * project.gross_contract
+        backlog_amount = (backlog_month.percent_complete - prior_month.percent_complete) * project.gross_contract
       else
-        backlog_month.percent_complete * project.gross_contract
+        backlog_amount = backlog_month.percent_complete * project.gross_contract
       end
-    else
-      0
     end
+
+    backlog_amount
   end
 
   def pipeline_predictions_for_projects(projects, month)
@@ -64,20 +64,25 @@ class MonthRangeWorker
       predictions << prediction
     end
 
+    #puts predictions.join(",")
+
     predictions
 
   end
 
   def pipeline_prediction_for_project(project, month)
     #only roll probability if the month is in the project
-    if project.start_on.present? and month >= project.start_on and month <= project.start_on.advance(months: project.months)
-      #roll the dice on the probability of the project and if we hit it then return
-      #the contract divided by number of months
-      if @random_number_generator.rand < project.probability
-        project.consulting_revenue / project.months
-      else
-        0
-      end
+    if month.end_of_month >= project.start_on and 
+       month.beginning_of_month <= project.start_on.advance(months: project.months) and
+       project.consulting_revenue.to_f > 0
+        #roll the dice on the probability of the project and if we hit it then return
+        #the contract divided by number of months
+        random = @random_number_generator.rand
+        if random < project.probability
+          project.consulting_revenue / project.months.to_f
+        else
+          0
+        end
     else
       0
     end
