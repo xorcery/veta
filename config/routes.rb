868fb5cb1d3c7c35a6e005e023f6a37ebd39d34f
Bryce Hammond <bryce.hammond@imulus.com>
2015-01-14 23:23:39 -0700

    Add routes and controller hooku pto change password

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :skip => [:registrations]                                          
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+      put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'            
+    end
 
   resources :pipeline, only: [:index, :new, :create]
   resources :backlog, only: [:index, :new, :create]
